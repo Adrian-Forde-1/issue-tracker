21945752c8bba25ab125232a3f7ca70f95b9e7f7
Adrian <AdrianForde02@gmail.com>
2020-05-16 21:04:03 -0300

    Changed server.js again

--- server.js
+++ server.js
@@ -43,8 +43,18 @@ if (process.env.NODE_ENV === 'production') {
   //Set static folder
   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // });
+
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'), function (
+      err
+    ) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    });
   });
 
   // app.get('/*', (req, res) => {
