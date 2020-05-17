4169ab842c993097c857515df16b0aa4e9de1c1e
Adrian <AdrianForde02@gmail.com>
2020-05-16 22:30:45 -0300

    Changed the code of tha catch all in node js backend

--- server.js
+++ server.js
@@ -43,10 +43,6 @@ if (process.env.NODE_ENV === 'production') {
   //Set static folder
   app.use(express.static('client/build'));
 
-  // app.get('*', (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  // });
-
   // app.get('/*', (req, res) => {
   //   res.sendFile(path.join(__dirname, 'client/build/index.html'), function (
   //     err
@@ -61,12 +57,23 @@ if (process.env.NODE_ENV === 'production') {
 //Routes
 app.use('/api', require('./routes/UserRoutes'));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'path/to/your/index.html'), function (err) {
-    if (err) {
-      res.status(500).send(err);
+app.get('/*', (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, 'client', 'build', 'index.html'),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
     }
-  });
+  );
 });
+
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'path/to/your/index.html'), function (err) {
+//     if (err) {
+//       res.status(500).send(err);
+//     }
+//   });
+// });
 
 app.listen(PORT, console.log(`Server is up and running on port ${PORT}`));
