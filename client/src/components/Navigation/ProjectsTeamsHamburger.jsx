0b785250d1978754df879c7044a36833dc9dd281
Adrian <AdrianForde02@gmail.com>
2020-08-02 19:08:25 -0300

    Changed the svg being used as the icons for the nav hamburger on smaller displays

--- client/src/components/Navigation/ProjectsTeamsHamburger.jsx
+++ client/src/components/Navigation/ProjectsTeamsHamburger.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //React Router
 import { withRouter } from 'react-router-dom';
@@ -7,24 +7,56 @@ import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 function ProjectsTeamsHamburger(props) {
+  const [navOpen, setNavOpen] = useState(false);
   const toggleNavOpen = (e) => {
     document
       .querySelector('.projects-teams-hamburger')
       .classList.toggle('nav-open');
     document.querySelector('.side-nav').classList.toggle('nav-open');
 
-    if (e.target.parentElement.classList.contains('nav-open')) {
-      e.target.classList.remove('fa-bars');
-      e.target.classList.add('fa-times');
-    } else {
-      e.target.classList.remove('fa-times');
-      e.target.classList.add('fa-bars');
-    }
+    setNavOpen(!navOpen);
   };
 
   return (
-    <div className="projects-teams-hamburger">
-      <i className="fas fa-bars" onClick={toggleNavOpen}></i>
+    <div
+      className="projects-teams-hamburger"
+      id="nav-hamburger"
+      onClick={toggleNavOpen}
+      style={{ cursor: 'pointer' }}
+    >
+      {navOpen ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          focusable="false"
+          width="1em"
+          height="1em"
+          preserveAspectRatio="xMidYMid meet"
+          viewBox="0 0 36 36"
+          style={{ color: 'white', fontSize: '20px' }}
+        >
+          <path
+            className="clr-i-outline clr-i-outline-path-1"
+            d="M19.41 18l8.29-8.29a1 1 0 0 0-1.41-1.41L18 16.59l-8.29-8.3a1 1 0 0 0-1.42 1.42l8.3 8.29l-8.3 8.29A1 1 0 1 0 9.7 27.7l8.3-8.29l8.29 8.29a1 1 0 0 0 1.41-1.41z"
+            fill="currentColor"
+          />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          focusable="false"
+          width="1em"
+          height="1em"
+          preserveAspectRatio="xMidYMid meet"
+          viewBox="0 0 50 50"
+          style={{ color: 'white', fontSize: '20px' }}
+        >
+          <path d="M10 12h30v4H10z" fill="currentColor" />
+          <path d="M10 22h30v4H10z" fill="currentColor" />
+          <path d="M10 32h30v4H10z" fill="currentColor" />
+        </svg>
+      )}
     </div>
   );
 }
