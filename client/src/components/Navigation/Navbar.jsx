5254bbb60d24b8ee5f0b2049cd0dafbd531d5153
Adrian Forde <AdrianForde02@gmail.com>
2021-01-04 04:10:54 -0400

    Stopped rendering the navbar hamburger on the homepage navbar if the user is authenticated and changed the navbar styling to make it more responsive and look better

--- client/src/components/Navigation/Navbar.jsx
+++ client/src/components/Navigation/Navbar.jsx
@@ -36,6 +36,52 @@ function Navbar(props) {
     return <span>{props.user.username.toString().toUpperCase()[0]}</span>;
   };
 
+  const renderHamburger = () => {
+    if (authenticated === false) {
+      return (
+        <>
+          {navOpen ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              focusable="false"
+              width="1em"
+              height="1em"
+              className="hamburger"
+              preserveAspectRatio="xMidYMid meet"
+              viewBox="0 0 36 36"
+              onClick={toggleNavbar}
+            >
+              <path
+                className="clr-i-outline clr-i-outline-path-1"
+                d="M19.41 18l8.29-8.29a1 1 0 0 0-1.41-1.41L18 16.59l-8.29-8.3a1 1 0 0 0-1.42 1.42l8.3 8.29l-8.3 8.29A1 1 0 1 0 9.7 27.7l8.3-8.29l8.29 8.29a1 1 0 0 0 1.41-1.41z"
+                fill="currentColor"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              focusable="false"
+              width="1em"
+              height="1em"
+              className="hamburger"
+              preserveAspectRatio="xMidYMid meet"
+              viewBox="0 0 50 50"
+              onClick={toggleNavbar}
+            >
+              <path d="M10 12h30v4H10z" fill="currentColor" />
+              <path d="M10 22h30v4H10z" fill="currentColor" />
+              <path d="M10 32h30v4H10z" fill="currentColor" />
+            </svg>
+          )}
+        </>
+      );
+    }
+
+    return null;
+  };
+
   const toggleNavbar = () => {
     document.querySelector(".hamburger").classList.toggle("nav-open");
     document.querySelector(".nav-list").classList.toggle("nav-open");
@@ -53,41 +99,7 @@ function Navbar(props) {
         <Link to="/" className="logo">
           <NavbarLogoSVG classes={"navbar__logo"} />
         </Link>
-        {navOpen ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-            focusable="false"
-            width="1em"
-            height="1em"
-            className="hamburger"
-            preserveAspectRatio="xMidYMid meet"
-            viewBox="0 0 36 36"
-            onClick={toggleNavbar}
-          >
-            <path
-              className="clr-i-outline clr-i-outline-path-1"
-              d="M19.41 18l8.29-8.29a1 1 0 0 0-1.41-1.41L18 16.59l-8.29-8.3a1 1 0 0 0-1.42 1.42l8.3 8.29l-8.3 8.29A1 1 0 1 0 9.7 27.7l8.3-8.29l8.29 8.29a1 1 0 0 0 1.41-1.41z"
-              fill="currentColor"
-            />
-          </svg>
-        ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-            focusable="false"
-            width="1em"
-            height="1em"
-            className="hamburger"
-            preserveAspectRatio="xMidYMid meet"
-            viewBox="0 0 50 50"
-            onClick={toggleNavbar}
-          >
-            <path d="M10 12h30v4H10z" fill="currentColor" />
-            <path d="M10 22h30v4H10z" fill="currentColor" />
-            <path d="M10 32h30v4H10z" fill="currentColor" />
-          </svg>
-        )}
+        {renderHamburger()}
         {/* <i
           className="fas fa-bars hamburger"
           onClick={() => {
@@ -116,53 +128,52 @@ function Navbar(props) {
               </Link>
             </li>
           )}
-
-          {authenticated === true && (
-            <div className="navbar__right-container">
-              <div
-                className="navbar__profile-pic"
-                onClick={() => {
-                  if (document.querySelector("#navbar-dropdown")) {
-                    document
-                      .querySelector("#navbar-dropdown")
-                      .classList.toggle("open");
-                  }
-                }}
-              >
-                {renderImage()}
-              </div>
-              <div className="navbar__right-dropdown" id="navbar-dropdown">
-                <div className="navbar__right-dropdown__account-info">
-                  <div className="navbar__right-dropdown__account-info-left">
-                    <div className="navbar__right-dropdown__account-info-left__profile-pic">
-                      {renderImage()}
-                    </div>
-                  </div>
-                  <div className="navbar__right-dropdown__account-info-right">
-                    <span>{props.user.username}</span>
-                    <span>{props.user.email}</span>
+        </ul>
+        {authenticated === true && (
+          <div className="navbar__right-container">
+            <div
+              className="navbar__profile-pic"
+              onClick={() => {
+                if (document.querySelector("#navbar-dropdown")) {
+                  document
+                    .querySelector("#navbar-dropdown")
+                    .classList.toggle("open");
+                }
+              }}
+            >
+              {renderImage()}
+            </div>
+            <div className="navbar__right-dropdown" id="navbar-dropdown">
+              <div className="navbar__right-dropdown__account-info">
+                <div className="navbar__right-dropdown__account-info-left">
+                  <div className="navbar__right-dropdown__account-info-left__profile-pic">
+                    {renderImage()}
                   </div>
                 </div>
-                <ul>
-                  <li
-                    onClick={() => {
-                      props.history.push("/profile");
-                    }}
-                  >
-                    Profile
-                  </li>
-                  <li
-                    onClick={() => {
-                      props.logoutUser(props.history);
-                    }}
-                  >
-                    Logout
-                  </li>
-                </ul>
+                <div className="navbar__right-dropdown__account-info-right">
+                  <span>{props.user.username}</span>
+                  <span>{props.user.email}</span>
+                </div>
               </div>
+              <ul>
+                <li
+                  onClick={() => {
+                    props.history.push("/profile");
+                  }}
+                >
+                  Profile
+                </li>
+                <li
+                  onClick={() => {
+                    props.logoutUser(props.history);
+                  }}
+                >
+                  Logout
+                </li>
+              </ul>
             </div>
-          )}
-        </ul>
+          </div>
+        )}
       </div>
     );
   } else {
