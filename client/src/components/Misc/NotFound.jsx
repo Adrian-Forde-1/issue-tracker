df62e58247e73db4d42c37c3b1379f4aa7133474
Adrian Forde <AdrianForde02@gmail.com>
2021-01-10 01:41:22 -0400

    Added an SVG and some information on the not found component

--- client/src/components/Misc/NotFound.jsx
+++ client/src/components/Misc/NotFound.jsx
@@ -1,14 +1,22 @@
 import React, { useEffect } from "react";
 
+//SVG
+import NoData from "../SVG/NoData";
+
 const NotFound = (props) => {
-  //   useEffect(() => {
-  //     if (props.location.pathname.indexOf("project") > -1) {
-  //       props.setCurrentProject("");
-  //     } else {
-  //       props.setCurrentTeam("");
-  //     }
-  //   }, []);
-  return <div>Not Found</div>;
+  useEffect(() => {
+    if (props.location.pathname.indexOf("project") > -1) {
+      props.setCurrentProject("");
+    } else {
+      props.setCurrentTeam("");
+    }
+  }, []);
+  return (
+    <div className="not-found__wrapper">
+      <NoData />
+      <h6>Opps, Sorry! Couldn't find what you're looking for.</h6>
+    </div>
+  );
 };
 
 export default NotFound;
